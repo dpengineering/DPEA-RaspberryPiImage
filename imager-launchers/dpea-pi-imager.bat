2a385df69f9d2033d3bde3fd25befe0173a065c0
Arnav Wadhwa <arnav.v.wadhwa@gmail.com>
2026-09-01 17:00:46 -0700

    chore: remove redundant comments

--- imager-launchers/dpea-pi-imager.bat
+++ imager-launchers/dpea-pi-imager.bat
@@ -1,4 +1,2 @@
 @echo off
-rem Windows: double-click to launch Raspberry Pi Imager preloaded with the DPEA
-rem image list, so "DPEA Pi" appears with the Customisation panel enabled.
 start "" "%ProgramFiles%\Raspberry Pi Imager\rpi-imager.exe" --repo https://github.com/dpengineering/DPEA-RaspberryPiImage/releases/latest/download/os-list.json
